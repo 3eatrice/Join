7d8f0da2112b46bfbe65fc4f2cb761e23e69a5c4
Beatrice Ambos <beatrice.ambos@web.de>
2023-05-08 10:08:00 +0200

    finished Version after approval

---
+++ js/logout.js
@@ -0,0 +1,65 @@
+
+/**
+ * Closes the logout container and redirects to index.html
+ */
+async function logout() {
+    currentUser = [];
+    await backend.setItem('currentUser', currentUser);
+    window.location.href = './index.html';
+}
+
+
+/**
+ * Shows the logout container
+ */
+function showLogOut() {
+    document.getElementById('logoutContainer').classList.add('logoutDisplayed');
+}
+
+
+/**
+ * Hides the logout container
+ */
+function hideLogOut() {
+    document.getElementById('logoutContainer').classList.remove('logoutDisplayed');
+}
+
+
+/**
+ * Shows the popup "Task added to board" with animation
+ */
+function activateLink(element) {
+    document.querySelectorAll('sideMenu-link-container-desktop').classList.remove('menuLinkActive');
+    element.classList.add('menuLinkActive');
+}
+
+/**
+ * w3-include-html tag javascript part. necessary for the template loading in html page.
+ */
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
+        }
+    }
+}
